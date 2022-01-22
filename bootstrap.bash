532a264d9ae4a79a7cafc67b8dd3314a003608bf
Ruairidh MacLeod <ruairidh@rkm.dev>
2022-01-22 21:23:38 +0000

    update bootstrap script

--- bootstrap.bash
+++ bootstrap.bash
@@ -21,13 +21,20 @@ echo "$USER ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/dont-ask-"$USER"-
 pushd /tmp
 
     curl --silent --location https://api.github.com/repos/rkm/personal-ansible/tarball > personal-ansible.tgz
-    TEMP_DIR="$(mktemp -d)"
-    tar xzf personal-ansible.tgz --strip 1 -C "$TEMP_DIR"
+    ANS_TEMP_DIR="$(mktemp -d)"
+    tar xzf personal-ansible.tgz --strip 1 -C "$ANS_TEMP_DIR"
 
-    pushd "$TEMP_DIR"
-        ./scripts/run_local.bash
+    curl --silent --location https://api.github.com/repos/rkm/dotfiles/tarball > dotfiles.tgz
+    DOT_TEMP_DIR="$(mktemp -d)"
+    tar xzf dotfiles.tgz --strip 1 -C "$DOT_TEMP_DIR"
+    cfg="$DOT_TEMP_DIR/linux/config/ansible.cfg"
+
+    pushd "$ANS_TEMP_DIR"
+        ANSIBLE_CONFIG="$cfg" ./scripts/run_local.bash
     popd
 
-    rm -rf "$TEMP_DIR" personal-ansible.tgz
+    rm -rf \
+        "$ANS_TEMP_DIR" personal-ansible.tgz \
+        "$DOT_TEMP_DIR" dotfiles.tgz
 
-popd # /tmp
+popd
