5fc9d1ab496461cbf8f47576c89d554d4e24c6bc
Ruairidh MacLeod <ruairidh@rkm.dev>
2022-01-22 21:21:27 +0000

    allow overriding anisble config

--- scripts/run_local.bash
+++ scripts/run_local.bash
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-ANSIBLE_CONFIG=/home/rkm/.config/ansible.cfg \
+ANSIBLE_CONFIG="${ANSIBLE_CONFIG:-/home/rkm/.config/ansible.cfg}" \
 ANSIBLE_HOST_KEY_CHECKING=False \
     /usr/bin/ansible-playbook \
     -i "localhost," \
